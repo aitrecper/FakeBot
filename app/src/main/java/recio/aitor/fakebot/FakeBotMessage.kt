1757a16d19000bad1484370f9d3a5d484760f79b
Aitor Reci Pérez <aitrecper@gmail.com>
2023-02-08 14:17:16 +0100

    Main Activity Layout

---
+++ app/src/main/java/recio/aitor/fakebot/FakeBotMessage.kt
@@ -0,0 +1,3 @@
+package recio.aitor.fakebot
+
+class FakeBotMessage(val id: String, val text: String, val user: Boolean )
--- app/src/main/java/recio/aitor/fakebot/MainActivity.kt
+++ app/src/main/java/recio/aitor/fakebot/MainActivity.kt
@@ -2,10 +2,24 @@ package recio.aitor.fakebot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import recio.aitor.fakebot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fbRecycler.layoutManager =LinearLayoutManager(this)
+        val fbChatList = mutableListOf<FakeBotMessage>()
+
+        if (fbChatList.isEmpty()){
+            binding.fbEmptyView.visibility = View.VISIBLE
+        }else{
+            binding.fbEmptyView.visibility = View.GONE
+        }
     }
 }
